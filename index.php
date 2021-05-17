9cdaaf570860aebfaa884e5ec879d3f531ce3add
Pvl72 <fcplm49@gmail.com>
2021-05-17 21:15:30 +0200

    init2

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>title</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+  </head>
+  <?php include nav.php ?>
+  <body>
+    <!-- page content -->
+  </body>
+</html>
---
+++ nav.php
@@ -0,0 +1,1 @@
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut, consequatur vero. Facilis alias necessitatibus tenetur et vero id voluptatem non sint dolores soluta doloribus nulla, nostrum praesentium magnam fugit quisquam?</p>
