bcfeb426905cbf1447d00321ad595dfaa2caabf0
Pvl72 <fcplm49@gmail.com>
2021-05-17 21:24:43 +0200

    init4

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <title>title</title>
-    <link rel="stylesheet" href="style.css">
-    <script src="script.js"></script>
-  </head>
-  <?php include nav.php ?>
-  <body>
+    <head>
+        <meta charset="utf-8">
+        <title>Connection</title>
+        <link rel="stylesheet" href="style.css">
+        <script src="script.js"></script>
+    </head>
+
+    <?php include "nav.php" ?>
+    
+    <body>
     <!-- page content -->
-  </body>
+    </body>
 </html>
--- nav.php
+++ nav.php
@@ -1,1 +1,3 @@
+<nav>
 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut, consequatur vero. Facilis alias necessitatibus tenetur et vero id voluptatem non sint dolores soluta doloribus nulla, nostrum praesentium magnam fugit quisquam?</p>
+</nav>
